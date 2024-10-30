a00731a8d900b85d6cd449335daa600add112ae8
Jesy05 <jgonzalezj@uamv.edu.ni>
2024-10-30 09:10:00 -0600

    Delete modulos/suma_resta_matrices.py
    esta malo

--- modulos/suma_resta_matrices.py
+++
@@ -1,9 +0,0 @@
-def sumar_matrices(A, B):
-    if len(A) != len(B) or len(A[0]) != len(B[0]):
-        raise ValueError("Las matrices deben tener las mismas dimensiones para sumarse.")
-    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
-
-def restar_matrices(A, B):
-    if len(A) != len(B) or len(A[0]) != len(B[0]):
-        raise ValueError("Las matrices deben tener las mismas dimensiones para restarse.")
-    return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
